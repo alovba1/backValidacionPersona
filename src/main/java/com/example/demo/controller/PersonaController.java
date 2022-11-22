342f8e76a06b11c11b71bdece5c6f1ad8504ec8f
alberto <curribifine@gmail.com>
2022-11-21 23:41:31 -0500

    se modifica back

--- src/main/java/com/example/demo/controller/PersonaController.java
+++ src/main/java/com/example/demo/controller/PersonaController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,8 @@ import com.example.demo.service.PersonaService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 @CrossOrigin
 @RestController
 @RequestMapping("/persona")
@@ -38,4 +43,21 @@ public class PersonaController {
 	public Persona add(@RequestBody Persona p ){
 		   return personaService.add(p);
     }
+	@GetMapping(path ={"/{id}"})
+	public Persona listarId(@PathVariable("id")String id) {
+	   return personaService.listarId(id);
+	}
+	
+	//@PutMapping()
+	@PutMapping("/{id}")
+	public Persona edit(@RequestBody Persona p, @PathVariable("id")String id){
+	  p.setId(id);
+		return personaService.edit(p);
+	}
+	@DeleteMapping
+	public ResponseEntity<?> delete(@PathVariable("id")String id){
+		personaService.delete(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT.value()).build();
+		}
+
 }
